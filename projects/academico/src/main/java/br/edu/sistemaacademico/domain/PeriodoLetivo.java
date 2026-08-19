a21d93af1d6aa01095fb92ae919027bbb6edeaf8
eduardo <eduardo.sacks@escola.pr.gov.br>
2026-08-18 21:52:49 -0300

    Implementa classes de domínio e validações com encapsulamento

---
+++ projects/academico/src/main/java/br/edu/sistemaacademico/domain/PeriodoLetivo.java
@@ -0,0 +1,38 @@
+package br.edu.sistemaacademico.domain;
+
+/**
+ * Representa o Período Letivo (a linha do tempo, não a do Trunks do Futuro).
+ */
+public class PeriodoLetivo {
+
+    private final int ano;
+    private final Semestre semestre;
+
+    public PeriodoLetivo(int ano, Semestre semestre) {
+        if (ano < 1900) { // Limite razoável para o ano
+            throw new IllegalArgumentException("Ano letivo inválido.");
+        }
+        if (semestre == null) {
+            throw new IllegalArgumentException("O semestre é obrigatório.");
+        }
+
+        this.ano = ano;
+        this.semestre = semestre;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public Semestre getSemestre() {
+        return semestre;
+    }
+
+    @Override
+    public String toString() {
+        return "PeriodoLetivo{" +
+                "ano=" + ano +
+                ", semestre=" + semestre +
+                '}';
+    }
+}
