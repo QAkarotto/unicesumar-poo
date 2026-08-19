391f18524ad15acd09688bf61c369f6c991c5121
Thaylor <thaylor911@gmail.com>
2026-08-18 23:51:15 -0300

    Remove comments from Semestre enum
    Removed comments about semester choices in Dragon Ball.

--- atividades/src/main/java/br/edu/sistemaacademico/domain/Semestre.java
+++ atividades/src/main/java/br/edu/sistemaacademico/domain/Semestre.java
@@ -1,7 +1,6 @@
 package br.edu.sistemaacademico.domain;
 
-// Até no universo de Dragon Ball existem escolhas:
-// aqui o sistema aceita apenas dois semestres.
+
 public enum Semestre {
     PRIMEIRO,
     SEGUNDO
