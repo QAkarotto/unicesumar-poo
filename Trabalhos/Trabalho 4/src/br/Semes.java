67bb23cf181c1c247b40a3fcf57dc376306f720e
Kauã Morais <kauamoraiscontato@gmail.com>
2026-08-20 21:40:00 -0300

    Dupla Kauã Morais e João Passos implementa sistema universitário, 1 - Quais validações foram adicionadas para proteger os objetos? R - Foi utilizado private para as  variáveis reservadas   não  possibilitando  o acesso direto das variáveis 2 - Quais informações podem ser alteradas e quais permanecem imutáveis? R - Nome, ID, e-mail, disciplina, período e semestre podem ser alterados usando os métodos set. 3 - Como os métodos de acesso e alteração ajudam a preservar o estado dos objetos? R - Os getters servem para consultar as informações e os setters para alterar os dados, sem precisar acessar as variáveis diretamente. Isso ajuda a manter os objetos mais organizados e protegidos.

---
+++ Trabalhos/Trabalho 4/src/br/Semes.java
@@ -0,0 +1,15 @@
+package br;
+
+public class Semes {
+    public boolean aceito;
+    public Semes (boolean aceito){
+        this.aceito=aceito;
+    }
+    public boolean getAceito(){
+        return aceito;
+    }
+    public void setAceito(boolean aceito) {
+        this.aceito=aceito;
+    }
+}
+
