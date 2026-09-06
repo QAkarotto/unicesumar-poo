6901665a462e49420ca5d4c990fad9ea6e41a3ff
Bruno de Carvalho <brunodecarvalho2117@gmail.com>
2026-09-06 19:15:35 -0300

    Implementa testes

---
+++ projects/academico/src/test/java/br/edu/sistemaacademico/domain/SistemaacademicoTest.java
@@ -0,0 +1,13 @@
+
+
+package br.edu.sistemaacademico;
+
+import org.junit.jupiter.api.Test;
+
+class SistemaAcademicoTest {
+
+    @Test
+    void deveExecutarCenarioCompletoSemErros() {
+        SistemaAcademico.main(new String[]{});
+    }
+}
