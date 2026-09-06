33cd8e2bb2e20f5634a38a20bcadb76352d5b9c3
joaoDV <jn3235877@gmail.com>
2026-09-06 19:29:10 -0300

    https://github.com/QAkarotto/unicesumar-poo/pull/191 1. Quais comportamentos principais foram testados? Foram testados os principais comportamentos do sistema acadêmico: oferta de disciplinas para uma turma, impedimento de ofertar a mesma disciplina duas vezes, matrícula de alunos, impedimento de matrícula duplicada na mesma oferta e controle do histórico acadêmico. Também foi testado que um aluno pode se matricular novamente em uma disciplina após ser reprovado, mas não pode se matricular novamente depois de ser aprovado. 2. Qual cobertura foi atingida? Foram criados 7 testes automatizados utilizando JUnit 5, cobrindo os principais fluxos e regras de negócio das classes Aluno, Turma, OfertaDisciplina, Matricula, Disciplina e PeriodoLetivo. Os testes verificam tanto situações de sucesso quanto situações em que o sistema deve impedir uma operação por meio de exceções. 3. Qual regra do sistema você considera mais importante manter protegida por testes? A regra mais importante é impedir que um aluno que já foi aprovado em uma disciplina possa se matricular novamente nela. Essa regra é importante porque evita uma matrícula indevida depois que o aluno já concluiu a disciplina com aprovação. O sistema verifica o histórico do aluno e lança uma exceção quando essa situação acontece.

---
+++ projects/academico/src/main/java/br/edu/sistemaacademico/Sitema academico atualizado/src/br/edu/sistemaacademico/domain/ResultadoAcademico.java
@@ -0,0 +1,7 @@
+package br.edu.sistemaacademico.domain;
+
+public class ResultadoAcademico {
+    public static final String CURSANDO = "Cursando";
+    public static final String APROVADO = "Aprovado";
+    public static final String REPROVADO = "Reprovado";
+}
