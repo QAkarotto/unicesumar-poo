c2dc5546628db0142d84446cb904a2ebf3e13c7c
Julio Vargas <115955332+JVargasDevCode@users.noreply.github.com>
2026-09-15 21:43:13 -0300

    Update SemestreTest.java

--- projects/academico/src/test/java/br/edu/sistemaacademico/SemestreTest.java
+++ projects/academico/src/test/java/br/edu/sistemaacademico/SemestreTest.java
@@ -1,4 +1,18 @@
-public enum Semestre {
-    PRIMEIRO,
-    SEGUNDO
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class SemestreTest {
+
+    @Test
+    @DisplayName("Deve conter exatamente os valores PRIMEIRO e SEGUNDO")
+    void deveConterTodosOsValores() {
+        var semestres = Semestre.values();
+
+        assertEquals(2, semestres.length);
+        assertEquals(Semestre.PRIMEIRO, Semestre.valueOf("PRIMEIRO"));
+        assertEquals(Semestre.SEGUNDO, Semestre.valueOf("SEGUNDO"));
+    }
 }
